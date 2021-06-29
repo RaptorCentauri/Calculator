23a7bd49a9aae3534bc834b58763f94b33b51327
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-06-29 23:12:41 +0000

    add further tests

--- __tests__/calculator.spec.js
+++ __tests__/calculator.spec.js
@@ -10,6 +10,16 @@ describe('calculator', function () {
 		let solution = calculator('15/5')
 		expect(solution).toBe(3)
 	})
+
+	test('the equation (5*3)^2+14/7*(125) should be 475', () => {
+		let solution = calculator('(5*3)^2+14/7*125')
+		expect(solution).toBe(475)
+	})
+	
+	test('the equation 6*(2+3) should be 30', () => {
+		let solution = calculator('6*(2+3)')
+		expect(solution).toBe(30)
+	})
 	
 	test('should return a number', () => {
 		let solution = calculator('3+5')
@@ -17,3 +27,4 @@ describe('calculator', function () {
 	})
 	
 })
+
--- __tests__/solveExpression.spec.js
+++ __tests__/solveExpression.spec.js
@@ -6,6 +6,11 @@ describe('solveExpression', function () {
 		expect(solution).toBe(8)
 	})
 	
+	test('should multiply two positive integers', () => {
+		let solution = solveExpression('5*7')
+		expect(solution).toBe(35)
+	})
+	
 	test('should return a number', () => {
 		let solution = solveExpression('3+5')
 		expect(isNaN(solution)).toBeFalsy()
