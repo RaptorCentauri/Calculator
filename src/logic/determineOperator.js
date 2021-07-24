d780a48287e60a6ce0c94b97e3f2e56b7df099d6
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-07-24 17:56:56 +0000

    move calculator into logic folder

---
+++ src/logic/determineOperator.js
@@ -0,0 +1,30 @@
+/**
+ * Returns a regular expression for the highest level operator in a mathematical expression using order of operations.
+ * @param {string} str - A mathematical expression given as a string
+ */
+
+function determineOperator(str) {
+	const operatorRegex = {
+		hasAdditionOrSubtraction: /[+-]/g,
+		hasMutliplicationOrDivision: /[*/]/g,
+		hasExponent: /[\^]/g,
+	}
+
+	let determinedOperator
+
+	switch (true) {
+		case operatorRegex['hasExponent'].test(str):
+			determinedOperator = operatorRegex['hasExponent']
+			break
+		case operatorRegex['hasMutliplicationOrDivision'].test(str):
+			determinedOperator = operatorRegex['hasMutliplicationOrDivision']
+			break
+		case operatorRegex['hasAdditionOrSubtraction'].test(str):
+			determinedOperator = operatorRegex['hasAdditionOrSubtraction']
+			break
+	}
+
+	return determinedOperator
+}
+
+export default determineOperator
