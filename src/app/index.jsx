32a9c13a6b9ee79874f5680e5ca45cdfbe79fcb5
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-07-30 01:44:14 +0000

    add clear button

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -7,8 +7,8 @@ import calculator from '../logic/calculator'
 
 
 const buttonValues = {
-  numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ],
-  operators: ['+', '-', '*', '/', '(', ')', '^']
+  numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ],
+  operators: ['+', '-', '*', '/', '(', ')', '^', '.', '=', 'C']
 }
  
 
@@ -17,26 +17,39 @@ const App = () => {
   const {numbers, operators} = buttonValues;
   
   const [equation, setEquation] = React.useState('');
-  const [solution, setSolution] = React.useState('');
+  const [solution, setSolution] = React.useState();
 
-  const handleClick = value => setEquation(`${equation}${value}`)
-  
+  const handleClick = value => {
+    if(value === 'C'){
+      let clearEquation = '';
+      setEquation(clearEquation);
+    }
+    
+    else if(value === '='){
+      let solved = calculator(equation);
+      {/* setSolution(solved); */}
+      setEquation(solved);
+    }
+
+    else{
+      setEquation(`${equation}${value}`)
+    }
+  }
+    
+      
+      
   const Button = ({value}) => <p className='button-style' onClick={() => handleClick(value)}> {value}</p>
-  
-  handleSolver = () => {
-    let solved = calculator(equation);
-    setSolution(solved)
-  };
-  
+    
   handleClear = () => setEquation('');
 
   
   return(
     <React.Fragment>
-      <h1 onClick={handleClear}>EQUATION (Click to clear): {equation}</h1>
-      <h1 onClick={handleSolver}>SOLUTION: {solution}</h1>
+      <h1 className='equation'>{equation}</h1>
+      <div className='button-layout'>
       {numbers.map((num) => <Button key={num} value={num}/> )}
       {operators.map((op) => <Button key={op} value={op}/> )} 
+      </div>
     </React.Fragment>
   )
 }
